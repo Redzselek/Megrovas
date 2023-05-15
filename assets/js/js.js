9128af77fdd1be26ae640cc390ffbb5f7218dabe
Egyedi Róbert <egyedirobika@gmail.com>
2023-05-15 14:41:08 +0200

    Kész

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,6 +1,24 @@
-var lista = []
-var megrovas = false
+var hanyadik = 0
 function Feldolgoz()
 {
-    document.getElementById("diakneve")
+    nev = document.getElementById("diakneve").value
+    tantargy = document.getElementById("tantargy").value
+    megrovas = document.getElementById("kere").checked
+    console.log(megrovas);
+    if (megrovas == true) 
+    {
+        hanyadik++
+        document.getElementById("elvett").innerHTML += '<option value="'+hanyadik+'">'+nev+'!!! ('+tantargy+')</option>'   
+    }
+    else
+    {
+        hanyadik++
+        document.getElementById("elvett").innerHTML += '<option value="'+hanyadik+'">'+nev+' ('+tantargy+')</option>' 
+    }
+
+}
+
+function Torol() 
+{
+   document.getElementById("elvett").remove(document.getElementById("elvett").selectedIndex)
 }
